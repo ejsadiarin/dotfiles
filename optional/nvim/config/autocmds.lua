2f9018c73d6e27037836856984f35a88b961e4c3
ejsadiarin <ejsadiarin@gmail.com>
2023-11-04 19:11:03 +0800

    added borders in hover lspconfig through autocmd

--- optional/nvim/config/autocmds.lua
+++ optional/nvim/config/autocmds.lua
@@ -1,3 +1,73 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.cmd([[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]])
+vim.cmd([[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]])
+
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = false,
+})
+
+-- -- LSP settings (for overriding per client)
+-- local handlers = {
+--   ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
+--   ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
+-- }
+
+-- Do not forget to use the on_attach function
+-- require("lspconfig").myserver.setup({ handlers = handlers })
+
+-- -- To instead override globally
+-- local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+-- function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+--   opts = opts or {}
+--   opts.border = opts.border or border
+--   return orig_util_open_floating_preview(contents, syntax, opts, ...)
+-- end
+
+-- require("lspconfig").myservertwo.setup({})
+
+local M = {}
+
+M.icons = {
+	Class = " ",
+	Color = " ",
+	Constant = " ",
+	Constructor = " ",
+	Enum = "了 ",
+	EnumMember = " ",
+	Field = " ",
+	File = " ",
+	Folder = " ",
+	Function = " ",
+	Interface = "ﰮ ",
+	Keyword = " ",
+	Method = "ƒ ",
+	Module = " ",
+	Property = " ",
+	Snippet = "﬌ ",
+	Struct = " ",
+	Text = " ",
+	Unit = " ",
+	Value = " ",
+	Variable = " ",
+}
+
+-- Change border of documentation hover window, See https://github.com/neovim/neovim/pull/13998.
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+	border = "rounded",
+})
+
+function M.setup()
+	local kinds = vim.lsp.protocol.CompletionItemKind
+	for i, kind in ipairs(kinds) do
+		kinds[i] = M.icons[kind] or kind
+	end
+end
+
+return M
