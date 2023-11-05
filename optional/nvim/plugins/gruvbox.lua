e3a4fa7758cd280a00c46c6d0cc6f1ad17d84887
ejsadiarin <ejsadiarin@gmail.com>
2023-11-06 01:47:04 +0800

    added transparent plugin to fix color mismatch

--- optional/nvim/plugins/gruvbox.lua
+++ optional/nvim/plugins/gruvbox.lua
@@ -103,6 +103,41 @@ return {
 		},
 	},
 
+	-- enable transparency so it will background will be the color of the terminal
+	-- ref: https://github.com/xiyaowong/transparent.nvim
+	-- command: :TransparentEnable --> will cached and transparent will be applied
+	{
+		"xiyaowong/transparent.nvim",
+		opts = {
+			groups = { -- table: default groups
+				"Normal",
+				"NormalNC",
+				"Comment",
+				"Constant",
+				"Special",
+				"Identifier",
+				"Statement",
+				"PreProc",
+				"Type",
+				"Underlined",
+				"Todo",
+				"String",
+				"Function",
+				"Conditional",
+				"Repeat",
+				"Operator",
+				"Structure",
+				"LineNr",
+				"NonText",
+				"SignColumn",
+				"CursorLineNr",
+				"EndOfBuffer",
+			},
+			extra_groups = {}, -- table: additional groups that should be cleared
+			exclude_groups = {}, -- table: groups you don't want to clear
+		},
+	},
+
 	-- {
 	--   "sainnhe/gruvbox-material",
 	--   lazy = false,
