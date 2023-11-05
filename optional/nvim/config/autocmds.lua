2b43f2dc6d6fc250b7b0420bca5bc5a3c4acd7fe
ejsadiarin <ejsadiarin@gmail.com>
2023-11-05 20:04:41 +0800

    fixed border color mismatch

--- optional/nvim/config/autocmds.lua
+++ optional/nvim/config/autocmds.lua
@@ -2,8 +2,8 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.cmd([[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]])
-vim.cmd([[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]])
+vim.cmd([[autocmd! ColorScheme * highlight NormalFloat guibg=#1d2021]])
+vim.cmd([[autocmd! ColorScheme * highlight FloatBorder guifg=#fbf1c7 guibg=#1d2021]])
 
 vim.diagnostic.config({
 	virtual_text = true,
