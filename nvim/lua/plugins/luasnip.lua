cd43b99091130e657df90ee72b3be48007390acf
ejsadiarin <ejsadiarin@gmail.com>
2024-01-03 22:20:35 +0800

    TODO: have custom snippets like prime

---
+++ nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+        require("luasnip.loaders.from_lua").lazy_load({ paths = "./lua/snippets" })
+      end,
+    },
+  },
+}
