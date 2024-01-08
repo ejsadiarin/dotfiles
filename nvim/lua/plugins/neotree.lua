829c5ec006370a7488f25c64874603aa93f3f548
ejsadiarin <ejsadiarin@gmail.com>
2024-01-08 14:18:14 +0800

    feat: netrw-like neotree but on steroids because why not its exquisite

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -5,8 +5,14 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     priority = 1000,
     opts = {
+      window = {
+        position = "current", -- values: 'left' | 'right' | 'top' | 'bottom' | 'float' | 'current' - like netrw
+        -- width = 40,
+      },
       filesystem = {
-        hijack_netrw_behavior = "open_default",
+        hijack_netrw_behavior = "open_current", -- like netrw but on steriods
+        -- [ if want file tree do ]: window.position = "left" or "right"
+        -- hijack_netrw_behavior = "open_default", -- defaults to what window.position is
         filtered_items = {
           visible = true,
           hide_dotfiles = false,
