f87ff4a8ccb8682f088477f3ecad630b31563d07
ejsadiarin <ejsadiarin@gmail.com>
2024-01-08 20:30:47 +0800

    feat(new): neotree moved to right
    since position = "current" doesn't "follow_focus_file" on open since it is just a buffer and current window temporarily "closes" while neo-tree instance is opened

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -3,21 +3,27 @@ local Util = require("lazyvim.util")
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
+    lazy = false,
     priority = 1000,
     opts = {
       window = {
-        position = "current", -- values: 'left' | 'right' | 'top' | 'bottom' | 'float' | 'current' - like netrw
+        -- position = "current", -- values: 'left' | 'right' | 'top' | 'bottom' | 'float' | 'current' - like netrw
+        position = "right",
         -- width = 40,
       },
       filesystem = {
-        hijack_netrw_behavior = "open_current", -- like netrw but on steriods
+        -- hijack_netrw_behavior = "open_current", -- like netrw but on steriods
         -- [ if want file tree do ]: window.position = "left" or "right"
-        -- hijack_netrw_behavior = "open_default", -- defaults to what window.position is
+        hijack_netrw_behavior = "open_default", -- defaults to what window.position is
         filtered_items = {
           visible = true,
           hide_dotfiles = false,
           hide_gitignored = false,
           -- hide_hidden = false, -- for Windows (use Linux bro)
+        },
+        follow_current_file = {
+          enabled = true,
+          leave_dirs_open = true,
         },
       },
     },
@@ -26,14 +32,22 @@ return {
         {
           "<leader>e",
           function()
-            require("neo-tree.command").execute({ toggle = true, dir = Util.root() })
+            require("neo-tree.command").execute({
+              toggle = true,
+              dir = Util.root(),
+              -- reveal_file = reveal_file,
+              -- reveal_force_cwd = true,
+            })
           end,
-          desc = "Explorer NeoTree (root dir)",
+          desc = "Explorer NeoTree (root/dynamic)",
         },
         {
           "<leader>E",
           function()
-            require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+            require("neo-tree.command").execute({
+              toggle = true,
+              dir = vim.loop.cwd(),
+            })
           end,
           desc = "Explorer NeoTree (cwd)",
         },
