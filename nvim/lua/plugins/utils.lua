77b415e3a742a7ba6536a4bcf402cf65a4620344
ejsadiarin <ejsadiarin@gmail.com>
2024-02-25 16:57:38 +0800

    chore: nvim updates

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -292,6 +292,14 @@ return {
       })
     end,
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 }
 -- icons = {
 --   misc = {
