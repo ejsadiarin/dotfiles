1c1334b076e512b57583240cfbc15c5d567fe2b4
ejsadiarin <ejsadiarin@gmail.com>
2024-02-26 14:02:06 +0800

    feat(new): added custom snippets for markdown for wizardry notes metadata

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -7,6 +7,9 @@ return {
       config = function()
         require("luasnip.loaders.from_vscode").lazy_load()
         require("luasnip.loaders.from_lua").lazy_load({ paths = "./lua/snippets" })
+        require("luasnip.loaders.from_snipmate").lazy_load({
+          paths = vim.fn.stdpath("config") .. "/lua/plugins/snippets",
+        })
       end,
     },
   },
