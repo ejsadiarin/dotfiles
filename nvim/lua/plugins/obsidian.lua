6eec5da0e69d8ba4e639021bb7cff7071c4e24f5
ejsadiarin <ejsadiarin@gmail.com>
2024-04-03 22:42:16 +0800

    new: snippets for obsidian support

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -97,22 +97,22 @@ return {
     -- },
     --
     -- -- Specify how to handle attachments.
-    -- attachments = {
-    --   -- The default folder to place images in via `:ObsidianPasteImg`.
-    --   -- If this is a relative path it will be interpreted as relative to the vault root.
-    --   -- You can always override this per image by passing a full path to the command instead of just a filename.
-    --   img_folder = "assets", -- This is the default
-    --   -- A function that determines the text to insert in the note when pasting an image.
-    --   -- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
-    --   -- This is the default implementation.
-    --   ---@param client obsidian.Client
-    --   ---@param path obsidian.Path the absolute path to the image file
-    --   ---@return string
-    --   img_text_func = function(client, path)
-    --     path = client:vault_relative_path(path) or path
-    --     return string.format("![%s](%s)", path.name, path)
-    --   end,
-    -- },
+    attachments = {
+      -- The default folder to place images in via `:ObsidianPasteImg`.
+      -- If this is a relative path it will be interpreted as relative to the vault root.
+      -- You can always override this per image by passing a full path to the command instead of just a filename.
+      img_folder = "assets", -- This is the default
+      -- A function that determines the text to insert in the note when pasting an image.
+      -- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
+      -- This is the default implementation.
+      ---@param client obsidian.Client
+      ---@param path obsidian.Path the absolute path to the image file
+      ---@return string
+      img_text_func = function(client, path)
+        path = client:vault_relative_path(path) or path
+        return string.format("![%s](%s)", path.name, path)
+      end,
+    },
   },
   keys = {
     {
@@ -149,6 +149,11 @@ return {
       "<leader>on",
       "<CMD>ObsidianNew<CR>",
       desc = "New Note",
+    },
+    {
+      "<leader>ol",
+      "<CMD>ObsidianLinks<CR>",
+      desc = "See all links in current note/buffer",
     },
   },
 }
