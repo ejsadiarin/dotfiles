d71c03f7a996b201c8e0c5761598975ee29e9496
ejsadiarin <ejsadiarin@gmail.com>
2024-04-05 23:05:21 +0800

    chore: added keymaps and things

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -158,46 +158,46 @@ return {
 
     -- -- Optional, configure additional syntax highlighting / extmarks.
     -- -- This requires you have `conceallevel` set to 1 or 2. See `:help conceallevel` for more details.
-    -- ui = {
-    --   enable = true, -- set to false to disable all additional syntax features
-    --   update_debounce = 200, -- update delay after a text change (in milliseconds)
-    --   -- Define how various check-boxes are displayed
-    --   checkboxes = {
-    --     -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
-    --     [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
-    --     ["x"] = { char = "", hl_group = "ObsidianDone" },
-    --     [">"] = { char = "", hl_group = "ObsidianRightArrow" },
-    --     ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
-    --     -- Replace the above with this if you don't have a patched font:
-    --     -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
-    --     -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
-    --
-    --     -- You can also add more custom ones...
-    --   },
-    --   -- Use bullet marks for non-checkbox lists.
-    --   bullets = { char = "•", hl_group = "ObsidianBullet" },
-    --   external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
-    --   -- Replace the above with this if you don't have a patched font:
-    --   -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
-    --   reference_text = { hl_group = "ObsidianRefText" },
-    --   highlight_text = { hl_group = "ObsidianHighlightText" },
-    --   tags = { hl_group = "ObsidianTag" },
-    --   block_ids = { hl_group = "ObsidianBlockID" },
-    --   hl_groups = {
-    --     -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
-    --     ObsidianTodo = { bold = true, fg = "#f78c6c" },
-    --     ObsidianDone = { bold = true, fg = "#89ddff" },
-    --     ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
-    --     ObsidianTilde = { bold = true, fg = "#ff5370" },
-    --     ObsidianBullet = { bold = true, fg = "#89ddff" },
-    --     ObsidianRefText = { underline = true, fg = "#c792ea" },
-    --     ObsidianExtLinkIcon = { fg = "#c792ea" },
-    --     ObsidianTag = { italic = true, fg = "#89ddff" },
-    --     ObsidianBlockID = { italic = true, fg = "#89ddff" },
-    --     ObsidianHighlightText = { bg = "#75662e" },
-    --   },
-    -- },
-    --
+    ui = {
+      enable = true, -- set to false to disable all additional syntax features
+      update_debounce = 200, -- update delay after a text change (in milliseconds)
+      -- Define how various check-boxes are displayed
+      checkboxes = {
+        -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+        [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+        ["x"] = { char = "", hl_group = "ObsidianDone" },
+        [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+        ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+        -- Replace the above with this if you don't have a patched font:
+        -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+        -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+
+        -- You can also add more custom ones...
+      },
+      -- Use bullet marks for non-checkbox lists.
+      bullets = { char = "•", hl_group = "ObsidianBullet" },
+      external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+      -- Replace the above with this if you don't have a patched font:
+      -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+      reference_text = { hl_group = "ObsidianRefText" },
+      highlight_text = { hl_group = "ObsidianHighlightText" },
+      tags = { hl_group = "ObsidianTag" },
+      block_ids = { hl_group = "ObsidianBlockID" },
+      hl_groups = {
+        -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
+        ObsidianTodo = { bold = true, fg = "#f78c6c" },
+        ObsidianDone = { bold = true, fg = "#89ddff" },
+        ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+        ObsidianTilde = { bold = true, fg = "#ff5370" },
+        ObsidianBullet = { bold = true, fg = "#89ddff" },
+        ObsidianRefText = { underline = true, fg = "#c792ea" },
+        ObsidianExtLinkIcon = { fg = "#c792ea" },
+        ObsidianTag = { italic = true, fg = "#89ddff" },
+        ObsidianBlockID = { italic = true, fg = "#89ddff" },
+        ObsidianHighlightText = { bg = "#75662e" },
+      },
+    },
+
     -- -- Specify how to handle attachments.
     attachments = {
       -- The default folder to place images in via `:ObsidianPasteImg`.
@@ -233,11 +233,6 @@ return {
       desc = "Open Current File in Obsidian",
     },
     {
-      "<leader>oj",
-      "<CMD>Obsidian<CR>",
-      desc = "Journal",
-    },
-    {
       "<leader>os",
       "<CMD>ObsidianSearch<CR>",
       desc = "Search/Grep Vault",
@@ -266,7 +261,18 @@ return {
           prompt_prefix = "Journal | ",
         })
       end,
-      desc = "Find files from Home",
+      desc = "Find Journal Files",
+    },
+    {
+      "<leader>fj",
+      function()
+        local dir = vim.env.HOME .. "/vault/Personal/Journal"
+        require("telescope.builtin").find_files({
+          find_command = { "fd", "-tf", "--hidden", "--search-path", dir },
+          prompt_prefix = "Journal | ",
+        })
+      end,
+      desc = "Find Journal Files",
     },
   },
 }
