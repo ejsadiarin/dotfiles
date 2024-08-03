3b4f5a88944b87c094d5da1ad952856e6f64a33e
ejsadiarin <ekwtech.container720@passinbox.com>
2024-08-03 00:48:00 +0000

    chore(refactor): support lazygit

--- nvim-rewrite/init.lua
+++ nvim-rewrite/init.lua
@@ -147,10 +147,7 @@ vim.keymap.set('n', '<leader>cl', '<cmd>LspInfo<CR>', { desc = 'Open [L]SP Info'
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('t', '<C-c>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- nvim-rewrite/lua/plugins/lazygit.lua
+++ nvim-rewrite/lua/plugins/lazygit.lua
@@ -17,5 +17,17 @@ return {
     keys = {
       { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'Open LazyGit' },
     },
+    config = function()
+      -- vim.g.lazygit_floating_window_winblend = 0 -- transparency of floating window
+      -- vim.g.lazygit_floating_window_scaling_factor = 0.9 -- scaling factor for floating window
+      -- vim.g.lazygit_floating_window_border_chars = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' } -- customize lazygit popup window border characters
+      -- vim.g.lazygit_floating_window_use_plenary = 1 -- use plenary.nvim to manage floating window if available
+      -- vim.g.lazygit_use_neovim_remote = 1 -- fallback to 0 if neovim-remote is not installed
+      --
+      -- vim.g.lazygit_use_custom_config_file_path = 0 -- config file path is evaluated if this value is 1
+      -- vim.g.lazygit_config_file_path = '' -- custom config file path
+      -- -- OR
+      -- vim.g.lazygit_config_file_path = {} -- table of custom config file paths
+    end,
   },
 }
