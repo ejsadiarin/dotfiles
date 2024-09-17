04f4b7908ba0933f5133e57fdf4025a88bd71a3d
ejsadiarin <ekwtech.container720@passinbox.com>
2024-09-17 19:27:36 +0800

    feat(new): restore colors, add memory unload on inactive tabs firefox

--- misc/firefox-ult/user.js
+++ misc/firefox-ult/user.js
@@ -2,10 +2,10 @@
 user_pref("user.theme.dark.a", true); /*default dark mode*/
 user_pref("user.theme.light.a", false); /*default light mode*/
 user_pref("user.theme.dark.catppuccin", false);
-user_pref("user.theme.dark.catppuccin-mocha", false); /*by Draff*/
+user_pref("user.theme.dark.catppuccin-mocha", true); /*by Draff*/
 user_pref("user.theme.dark.gruvbox", false);
 user_pref("user.theme.light.gruvbox", false);
-user_pref("user.theme.dark.midnight", true);
+user_pref("user.theme.dark.midnight", false);
 
 /*tabs*/
 user_pref("ultima.tabs.vertical", true);
@@ -67,6 +67,7 @@ user_pref("devtools.debugger.prompt-connection", false);
 user_pref("svg.context-properties.content.enabled", true);
 user_pref("toolkit.tabbox.switchByScrolling", false);
 user_pref("browser.tabs.hoverPreview.enabled", true);
+user_pref("browser.tabs.unloadOnLowMemory", true);
 /*enable userchrome,
 disable about config warning,
 enable browsertoolbox,
