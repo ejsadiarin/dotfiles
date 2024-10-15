5ba4e4b9b9bdc95ccc3a750fe2febdf92769a271
ejsadiarin <ejsadiarin@gmail.com>
2024-10-15 22:10:10 +0800

    feat(remaps): remapped mini.surround keybinds to prefix 'g' ex. from 'sa' -> 'gsa' for instant flash.nvim with 's'
    blazingly fast 's'

--- nvim/lua/plugins/mini-modules.lua
+++ nvim/lua/plugins/mini-modules.lua
@@ -16,7 +16,20 @@ return {
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      require('mini.surround').setup {
+        mappings = {
+          add = 'gsa', -- Add surrounding in Normal and Visual modes
+          delete = 'gsd', -- Delete surrounding
+          find = 'gsf', -- Find surrounding (to the right)
+          find_left = 'gsF', -- Find surrounding (to the left)
+          highlight = 'gsh', -- Highlight surrounding
+          replace = 'gsr', -- Replace surrounding
+          update_n_lines = 'gsn', -- Update `n_lines`
+
+          suffix_last = 'l', -- Suffix to search with "prev" method
+          suffix_next = 'n', -- Suffix to search with "next" method
+        },
+      }
 
       require('mini.icons').setup {
         lazy = true,
