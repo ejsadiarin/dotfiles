bb16a6e08cdba39f5f34d40fe925bf81f24e8174
ejsadiarin <kyube.ejs@proton.me>
2024-10-24 16:04:21 +0800

    feat(goimports): auto clean import on go files on save

--- nvim/lua/extensions/go.lua
+++ nvim/lua/extensions/go.lua
@@ -1,6 +1,20 @@
 -- Configures additional things for Go programming language support
 -- --> specific lsp config is already configured in plugins/lspconfig.lua
 
+-- vim.cmd [[autocmd BufWritePre *.go lua vim.lsp.buf.format({ async = true })]]
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = '*.go',
+    callback = function()
+        vim.lsp.buf.code_action({
+            context = {
+                only = { 'source.organizeImports' }
+            },
+            apply = true
+        })
+    end
+})
+
 return {
 
     {
