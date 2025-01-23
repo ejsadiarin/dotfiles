f6179a646161f1a468646fd7f2d07f07303f0b1e
ejsadiarin <kyube.ejs@proton.me>
2025-01-23 23:30:32 +0800

    chore: fix issues

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -58,11 +58,6 @@ end
 -- tabs
 config.enable_tab_bar = false
 
-local mux = wezterm.mux
-
-wezterm.on("gui-startup", function(cmd)
-	local tab, pane, window = mux.spawn_window(cmd or {})
-	window:gui_window():toggle_fullscreen()
-end)
+-- gpu
 
 return config
