2a9b819c0aba31900f57b169da040b27dc193572
ejsadiarin <ejs@ejsadiarin.com>
2026-03-18 13:55:17 +0800

    feat(installer): add work-wsl install script

---
+++ work-install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# NOTE: zsh, tmux, neovim, opencode
+
+# install essentials
+echo "[INSTALL] Installing essential binaries"
+sudo apt install -y zsh vim tmux gpg gcc make git ripgrep fd-find unzip fzf openssh-server curl
+
+# NOTE: runtimes (node, bun, etc.)
+# instal nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+nvm install 24
+
+# symlink home configs
+echo "[WARN] Removing home configs"
+rm -f ~/.zshrc
+ln -s ~/dotfiles/.zshrc ~
+rm -f ~/.vimrc
+ln -s ~/dotfiles/.vimrc ~
+
+# rm -f ~/.gitconfig
+# ln -s ~/dotfiles/.gitconfig ~
+
+# NOTE: tmux
+echo "[WARN] Removing tmux configs: ~/.config/tmux"
+rm -rf ~/.config/tmux
+rm -f ~/.tmux.conf
+ln -s ~/dotfiles/tmux ~/.config
+ln -s ~/dotfiles/tmux/.tmux.conf ~
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+tmux source ~/.tmux.conf
+echo "[INFO] open tmux then do <ctrl+b> + I"
+
+# NOTE: neovim
+echo "[Install] neovim config"
+rm -rf ~/.config/nvim
+ln -s ~/dotfiles/nvim ~/.config
+# install rust and cargo for tree-sitter-cli
+curl https://sh.rustup.rs -sSf | sh
+cargo install --locked tree-sitter-cli
+echo "[INFO] open nvim and let it lazy install"
+
+# NOTE: opencode
+sudo npm i -g bun
+bun add -g opencode-ai
+echo "[WARN] Removing opencode config file: ~/.config/opencode/opencode.json"
+rm -f ~/.config/opencode/opencode.json
+ln -s ~/dotfiles/config/opencode/opencode.jsonc ~/.config/opencode
+ln -s ~/dotfiles/config/opencode/AGENTS.md ~/.config/opencode
